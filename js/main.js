757577b31640ef5008e740d1c9abe538f2d0a3bb
Michał Noszczyk <noszczykmichal@wp.pl>
2020-01-29 23:40:41 +0100

    Dwie  pierwsze liczby

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let btn=document.getElementById('generate');
+
+function pesel(){
+    let namePerson=document.getElementById('namePerson');
+    let imieNazwisko=document.getElementById('imieNazwisko');
+    namePerson.innerText=imieNazwisko.value
+    let date=document.getElementById('dataUrodzenia').value
+    let pesel=document.getElementById('pesel');
+    let pierwDruga=date.substring(0,2);
+    pesel.innerText=`${pierwDruga}`
+}
+
+
+
+
+
+btn.addEventListener('click', pesel);
