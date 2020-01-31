2fcfd1fb3b606e3b8ead0cdef6803545139ef959
Michał Noszczyk <noszczykmichal@wp.pl>
2020-01-31 21:04:43 +0100

    Liczba oznaczenie plci

--- js/main.js
+++ js/main.js
@@ -12,15 +12,42 @@ function pesel(){
     //trzecia i czwarta cyfra peselu
     let trzeciaCzwarta=parseInt(data.substring(5,7),10);
     //piąta i szósta
-    let piataSzósta=parseInt(data.substring(8,10),10);
+    let piataSzósta=data.substring(8,10);
     //trzy losowe
     function los(min,max){
         return Math.floor(Math.random()*(max-min)+min)
     }
     let trzyLos=los(100,999);
-    pesel.innerText=`${pierwDruga}${trzeciaCzwarta+20}${piataSzósta}${trzyLos}`
+    
+    //oznaczenie płci
+    let mezczyzna=document.getElementById('mezczyzna');
+    let kobieta=document.getElementById('kobieta');
+    let liczba=null;
+    let liczba2=null;
+    function plec(){
+        if(kobieta.checked=== true){
+            liczba=los(0,9);
+            
+            if(liczba % 2 == 0){
+                return liczba
+            }else{
+                return 0
+            }
+        }else if(mezczyzna.checked===true){
+            liczba2=los(1,10);
+            console.log(liczba2)
+            if(liczba2 % 2 != 0){
+                return liczba2
+            }else{
+                return 1
+            }    
+    }
 }
+    let liczbaOznPlci=plec();
+    pesel.innerText=`${pierwDruga}${trzeciaCzwarta+20}${piataSzósta}${trzyLos}${liczbaOznPlci}`
 
+    
+}
 
 
 
