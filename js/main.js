7a2a281d0391014feb7f6f4cc72493a731e563c5
Michal <noszczykmichal@wp.pl>
2022-04-12 06:23:56 +0200

    full code refactoring

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function calculateMonth(fullBirthDate, yearOfBirth) {
   if (yearOfBirth >= 1800 && yearOfBirth <= 1899) {
     return `${80 + monthOfBirth}`;
   } else if (yearOfBirth >= 1900 && yearOfBirth <= 1999) {
-    return monthOfBirth<=9? `0${monthOfBirth}` : `${monthOfBirth}`;
+    return monthOfBirth <= 9 ? `0${monthOfBirth}` : `${monthOfBirth}`;
   } else if (yearOfBirth >= 2000 && yearOfBirth <= 2099) {
     return `${20 + monthOfBirth}`;
   } else if (yearOfBirth >= 2100 && yearOfBirth <= 2199) {
@@ -25,10 +25,10 @@ function calculateMonth(fullBirthDate, yearOfBirth) {
   }
 }
 
-const drawNumbers = (min, max, numLenght=1) => {
-    const drawnNum=Math.floor(Math.random() * (max - min) + min);
+const drawNumbers = (min, max, numLenght = 1) => {
+  const drawnNum = Math.floor(Math.random() * (max - min) + min);
 
-    return (drawnNum<100 && numLenght===3)? `0${drawnNum}` : `${drawnNum}`;
+  return drawnNum < 100 && numLenght === 3 ? `0${drawnNum}` : `${drawnNum}`;
 };
 
 function genderDesignationHandler() {
@@ -42,6 +42,13 @@ function genderDesignationHandler() {
 
     return number % 2 !== 0 ? number : 1;
   }
+}
+
+function calculateControlNum(...allNums) {
+  //summing up all numbers that were got from multiplying individual numbers from Pesel number by their weight; sum that was calculated is then devided by modulo of 10; on the Government's' site there is no clear info what happens if modulo of 10 from sum gives 0; according to the info found on the Internet then the control number equals 0
+  const sumByModule10 = allNums.reduce((curr, prev) => curr + prev) % 10;
+
+  return sumByModule10 === 0 ? 0 : 10 - sumByModule10;
 }
 
 function liczPesel() {
@@ -66,86 +73,74 @@ function liczPesel() {
   const sixthPeselNum = +dayToPesel.substring(1);
   const seventhPeselNum = +freeRandom.substring(0, 1);
   const eighthPeselNum = +freeRandom.substring(1, 2);
-  const ninethPeselNum = +freeRandom.substring(2, 3);
+  const ninthPeselNum = +freeRandom.substring(2, 3);
   const tenthPeselNum = genderDesigNumber;
 
-  console.log({ firstPeselNum });
-  console.log({ secondPeselNum });
-  console.log({ thirdPeselNum });
-  console.log({ fourthPeselNum });
-  console.log({ fithPeselNum });
-  console.log({ sixthPeselNum });
-
   console.log(
-    `${firstPeselNum}${secondPeselNum}${thirdPeselNum}${fourthPeselNum}${fithPeselNum}${sixthPeselNum}${seventhPeselNum}${eighthPeselNum}${ninethPeselNum}${tenthPeselNum}`
+    `${firstPeselNum}${secondPeselNum}${thirdPeselNum}${fourthPeselNum}${fithPeselNum}${sixthPeselNum}${seventhPeselNum}${eighthPeselNum}${ninthPeselNum}${tenthPeselNum}`
   );
 
-  let dziesiatPesel = liczbaOznPlci;
+  //caclulation of the control number- each of ten previously calculated numbers that make up Pesel number has to be multiplied by given weight
 
-  //weryfikacja czy kolejne liczby z Peselu przemnożone przez kolejną wagę są jedno czy dwu cyfrowe
-  let pierwKontr = pierPesel * 1;
-  let drugaKontr =
-    drugaPesel * 3 >= 10
-      ? parseInt((drugaPesel * 3).toString().substring(1, 2), 10)
-      : drugaPesel * 3;
+  const firstToControl = firstPeselNum * 1;
+  const secondToControl =
+    secondPeselNum * 3 >= 10
+      ? +(secondPeselNum * 3).toString().substring(1)
+      : secondPeselNum * 3;
+  const thirdToControl =
+    thirdPeselNum * 7 >= 10
+      ? +(thirdPeselNum * 7).toString().substring(1)
+      : thirdPeselNum * 7;
+  const fourthToControl =
+    fourthPeselNum * 9 >= 10
+      ? +(fourthPeselNum * 9).toString().substring(1)
+      : fourthPeselNum * 9;
+  const fithToControl = fithPeselNum * 1;
+  const sixthToControl =
+    sixthPeselNum * 3 >= 10
+      ? +(sixthPeselNum * 3).toString().substring(1)
+      : sixthPeselNum * 3;
+  const seventhToControl =
+    seventhPeselNum * 7 >= 10
+      ? +(seventhPeselNum * 7).toString().substring(1)
+      : seventhPeselNum * 7;
+  const eighthToControl =
+    eighthPeselNum * 9 >= 10
+      ? +(eighthPeselNum * 9).toString().substring(1)
+      : eighthPeselNum * 9;
+  const ninthToControl = ninthPeselNum * 1;
+  const tenthToControl =
+    tenthPeselNum * 3 >= 10
+      ? +(tenthPeselNum * 3).toString().substring(1)
+      : tenthPeselNum * 3;
 
-  let trzeKontr =
-    trzeciaPesel * 7 >= 10
-      ? parseInt((trzeciaPesel * 7).toString().substring(1, 2), 10)
-      : trzeciaPesel * 7;
-  let czwartKontr =
-    czwartaPesel * 9 >= 10
-      ? parseInt((czwartaPesel * 9).toString().substring(1, 2), 10)
-      : czwartaPesel * 9;
-  let piataKontr = piataPesel * 1;
-  let szostaKontr =
-    szostaPesel * 3 >= 10
-      ? parseInt((szostaPesel * 3).toString().substring(1, 2), 10)
-      : szostaPesel * 3;
-  let siodmaKontr =
-    siodmaPesel * 7 >= 10
-      ? parseInt((siodmaPesel * 7).toString().substring(1, 2), 10)
-      : siodmaPesel * 7;
-  let osmaKontr =
-    osmaPesel * 9 >= 10
-      ? parseInt((osmaPesel * 9).toString().substring(1, 2), 10)
-      : osmaPesel * 9;
-  let dziewKontr = dziewiatPesel * 1;
-  let dziesiatKontr =
-    dziesiatPesel * 3 >= 10
-      ? parseInt((dziesiatPesel * 3).toString().substring(1, 2), 10)
-      : dziesiatPesel * 3;
+  console.log({ firstToControl });
+  console.log({ secondToControl });
+  console.log({ thirdToControl });
+  console.log({ fourthToControl });
+  console.log({ fithToControl });
+  console.log({ sixthToControl });
+  console.log({ seventhToControl });
+  console.log({ eighthToControl });
+  console.log({ ninthToControl });
+  console.log({ tenthToControl });
 
-  function liczKontrolna() {
-    //dodajemy sumę wszystkich liczb z peselu przemnożonych przez wagę
-    let sumaWag =
-      pierwKontr +
-      drugaKontr +
-      trzeKontr +
-      czwartKontr +
-      piataKontr +
-      szostaKontr +
-      siodmaKontr +
-      osmaKontr +
-      dziewKontr +
-      dziesiatKontr;
-    console.log("TCL: liczKontrolna -> sumaWag", sumaWag);
-    //zmiana logiki wyliczania liczby kontrolnej-> strona ministerialna nie podaje co jeśli modulo z 10 dla sumy wag poszczególnych liczb z Peselu wynosi 0-> wtedy odejmowanie od 10 daje 10; według informacji z internetu dla takiej sytuacji jako cyfrę kontrokną należy przyjąć 0
-    let sumaWagModulo10 = sumaWag % 10;
-    console.log("sumaWagModulo10", sumaWagModulo10);
+  const controlNum = calculateControlNum(
+    firstToControl,
+    secondToControl,
+    thirdToControl,
+    fourthToControl,
+    fithToControl,
+    sixthToControl,
+    seventhToControl,
+    eighthToControl,
+    ninthToControl,
+    tenthToControl
+  );
 
-    if (sumaWagModulo10 == 0) {
-      return 0;
-    } else {
-      return 10 - sumaWagModulo10;
-    }
-  }
-
-  let liczbaKontrolna = liczKontrolna();
-  console.log("liczbaKontrolna:", liczbaKontrolna);
+  console.log({ controlNum });
 
   //wstrzykiwanie obliczonej zawartości do spana 'Pesel'
-  pesel.innerText = `${pierwDruga}${trzeciaCzwarta}${piataSzosta}${trzyLos}${liczbaOznPlci}${liczbaKontrolna}`;
+//   pesel.innerText = `${pierwDruga}${trzeciaCzwarta}${piataSzosta}${trzyLos}${liczbaOznPlci}${liczbaKontrolna}`;
 }
-
 generate.addEventListener("click", liczPesel);
